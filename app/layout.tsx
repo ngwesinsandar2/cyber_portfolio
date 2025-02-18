62b2c25b631a1fc1d0087b6f35cf4cd4d2306927
Ngwe Sin Sandar <ngwesinsandar112@gmail.com>
2025-02-19 01:54:12 +0630

    feat: changed meta data location

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,23 +3,23 @@ import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
-export const metadata = {
-  title: 'Ngwe Sin Sandar',
-  description: 'This is Cyber Portfolio with NextJs, TypeScript and Tailwind',
-  openGraph: {
-    type: 'website',
-    url: 'https://ngwesinsandarnext.vercel.app',
-    title: 'Ngwe Sin Sandar',
-    description: 'This is Cyber Portfolio with NextJs, TypeScript and Tailwind',
-    images: [
-      {
-        url: './opengraph-image.png',
-        width: 1200,
-        height: 630
-      }
-    ]
-  }
-};
+// export const metadata = {
+//   title: 'Ngwe Sin Sandar',
+//   description: 'This is Cyber Portfolio with NextJs, TypeScript and Tailwind',
+//   openGraph: {
+//     type: 'website',
+//     url: 'https://ngwesinsandarnext.vercel.app',
+//     title: 'Ngwe Sin Sandar',
+//     description: 'This is Cyber Portfolio with NextJs, TypeScript and Tailwind',
+//     images: [
+//       {
+//         url: './opengraph-image.png',
+//         width: 1200,
+//         height: 630
+//       }
+//     ]
+//   }
+// };
 
 type Props = {
   children: ReactNode;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
 import { redirect } from 'next/navigation';
 
+export const metadata = {
+  title: 'Ngwe Sin Sandar',
+  description: 'This is Cyber Portfolio with NextJs, TypeScript and Tailwind',
+  openGraph: {
+    type: 'website',
+    url: 'https://ngwesinsandarnext.vercel.app',
+    title: 'Ngwe Sin Sandar',
+    description: 'This is Cyber Portfolio with NextJs, TypeScript and Tailwind',
+    images: [
+      {
+        url: './opengraph-image.png',
+        width: 1200,
+        height: 630
+      }
+    ]
+  }
+};
+
 // This page only renders when the app is built statically (output: 'export')
 export default function RootPage() {
   redirect('/en');
