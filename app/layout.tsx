b29de6fb337e50bf74c931b7ea164e0bf8c54eaa
Ngwe Sin Sandar <ngwesinsandar112@gmail.com>
2025-02-19 01:35:16 +0630

    feat: added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Ngwe Sin Sandar',
@@ -26,5 +27,10 @@ type Props = {
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
 export default function RootLayout({ children }: Props) {
-  return children;
+  return (
+    <>
+      {children}
+      <Analytics />
+    </>
+  );
 }
