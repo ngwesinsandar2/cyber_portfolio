362581d2c563d4d04b85972e2627c8240f0d828f
Ngwe Sin Sandar <ngwesinsandar112@gmail.com>
2025-02-19 01:24:29 +0630

    feat: added favicon and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,22 @@
-import {ReactNode} from 'react';
+import { ReactNode } from 'react';
 import './globals.css';
 
 export const metadata = {
-  title: "Ngwe Sin Sandar",
-  description: "This is Cyber Portfolio with NextJs, TypeScript and Tailwind",
+  title: 'Ngwe Sin Sandar',
+  description: 'This is Cyber Portfolio with NextJs, TypeScript and Tailwind',
+  openGraph: {
+    type: 'website',
+    url: 'https://ngwesinsandarnext.vercel.app',
+    title: 'Ngwe Sin Sandar',
+    description: 'This is Cyber Portfolio with NextJs, TypeScript and Tailwind',
+    images: [
+      {
+        url: 'https://ngwesinsandar.com/og-image.png',
+        width: 1200,
+        height: 630
+      }
+    ]
+  }
 };
 
 type Props = {
@@ -12,6 +25,6 @@ type Props = {
 
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
-export default function RootLayout({children}: Props) {
+export default function RootLayout({ children }: Props) {
   return children;
 }
